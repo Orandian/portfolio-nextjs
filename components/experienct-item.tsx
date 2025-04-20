3dbb5793ae59e516043b425ca4fe5784aff93214
kyaw-starse <101231054+kyaw-starse@users.noreply.github.com>
2025-04-20 15:41:42 +0900

    update in list for experience

--- components/experienct-item.tsx
+++ components/experienct-item.tsx
@@ -12,7 +12,7 @@ const ExperienceItem: React.FC<ExperienceItemProps> = ({ title, date, location, 
     <div className="mb-10">
       <h3 className="text-xl font-semibold">{title}</h3>
       <p className="text-sm text-gray-500">{date} | {location}</p>
-      <ul className="mt-3 list-disc list-inside space-y-1 text-base text-gray-700">
+      <ul className="mt-3 pl-3.5 list-disc space-y-1 text-base text-gray-700">
         {points.map((point, index) => (
           <li key={index}>{point}</li>
         ))}
